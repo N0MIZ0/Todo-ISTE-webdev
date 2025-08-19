db15252a50dae680e3a610b508b247448f4f7a4b
Ad <75129167+N0MIZ0@users.noreply.github.com>
2025-08-19 11:08:00 +0530

    comments

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
-// tells vite to use js + handles react-jsx + fast refresh
+// tells vite to use js + handles react-jsx + fast auto refresh
