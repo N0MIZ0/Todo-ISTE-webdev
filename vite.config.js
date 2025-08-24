7938f23ed711073d229df390478670ed7f9b5a66
n0miz0 <advait.balachandar1537@gmail.com>
2025-08-24 14:33:37 +0530

    Updated index

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,3 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
-<<<<<<< HEAD
-// tells vite to use js + handles react-jsx + fast auto refresh
-=======
->>>>>>> master
